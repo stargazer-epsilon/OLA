506040c7da53d796189c4e5d220842ef3428750f
Jeffrey Ting <jeffrey.ting@icloud.com>
2023-06-09 18:14:09 -0500

    Added initial venv / requirements and .py file

---
+++ ResumeScrape.py
@@ -0,0 +1,20 @@
+import selenium
+import pandas as pd
+import numpy as np
+import time
+import os
+import re
+import sys
+import csv
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# Set up the driver
+driver = webdriver.Chrome()
+
+
+
+# Download the pdf found on the website
+
+
