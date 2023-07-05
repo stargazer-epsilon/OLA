08e9bbf75c80a79126272775d60005357ebdb982
Jeffrey Ting <jeffrey.ting@icloud.com>
2023-07-05 07:59:32 -0500

    update

--- ResumeScrape.py
+++ ResumeScrape.py
@@ -1,20 +1,30 @@
-import selenium
 import pandas as pd
-import numpy as np
-import time
 import os
-import re
-import sys
-import csv
 
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+# File name stored in variable
+input_file_name = "Export_68415514AE688183BDA813BABC41AD54B0E15995.csv"
 
-# Set up the driver
-driver = webdriver.Chrome()
+# Construct the path to the Downloads directory for Windows
+downloads_path = os.path.join(os.path.expanduser("~"), "Downloads", input_file_name)
 
+# Read in the CSV file to a dataframe
+df = pd.read_csv(downloads_path)
 
+# Remove any records that don't have "Yes" under the column 'OLAPermissionShareResume'
+df = df.loc[df['OLAPermissionShareResume'] == 'Yes']
 
-# Download the pdf found on the website
+# Create two new columns to extract the text to the right of the character "|" from the columns ResumePDF and ResumeWord
+df['ResumePDF_Right'] = df['ResumePDF'].str.split("|").str[1]
+df['ResumeWord_Right'] = df['ResumeWord'].str.split("|").str[1]
 
+# Keep only the specific columns
+df = df[['ProspectClassification', 'ResumePDF_Right', 'ResumeWord_Right', 'Email_Address', 'First_Name', 'Last_Name']]
 
+# Create a new dataframe for each category of 'ProspectClassification'
+dfs = {}
+for classification in df['ProspectClassification'].unique():
+    dfs[classification] = df[df['ProspectClassification'] == classification]
+
+# To access a dataframe, you can use its corresponding 'ProspectClassification' value as the key
+# For example, to access the dataframe for the classification 'Category1', you would use:
+# print(dfs['Category1'])
